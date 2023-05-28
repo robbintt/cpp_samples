2438770b1717fb56ea0caba270bb7ab117ceb21c
Trent Robbins <robbintt@gmail.com>
2023-05-28 10:56:43 -0700

    Update formatter to show all issues according to gpt4.

--- .github/workflows/format.sh
+++ .github/workflows/format.sh
@@ -3,11 +3,14 @@
 exit_status=0
 
 find . -name '*.cpp' -o -name '*.h' | while read -r file; do
-  diff -q <(clang-format "$file") "$file" > /dev/null
+  diff_output=$(diff -u <(clang-format "$file") "$file")
   if [ $? -ne 0 ]; then
-    echo "$file is not properly formatted"
+    echo "Formatting issues in $file:"
+    echo "$diff_output"
+    echo ""
     exit_status=1
   fi
 done
 
 exit $exit_status
+
