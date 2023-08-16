a53b87b085acbf6a6077ec321850ded536c00769
Trent Robbins <robbintt@gmail.com>
2023-08-15 19:42:47 -0700

    Add in required functions for vector

--- sorting/src/tour_vector.cpp
+++ sorting/src/tour_vector.cpp
@@ -8,23 +8,31 @@ class Vector {
 public:
   Vector(int s)
     : elem{new double[s]},
-    sz{s}
+    _size{s}
   {
+    // not sure if should reserve size or nearest 2^i
+    reserve(_size);
+    // TODO why initialize as zero here and not as asdf[]{0} or something?
     for (int i=0; i!=s; ++i)
       elem[i]=0;
   }
 
+  // need to ensure _capacity is reserve()'d before using
+  /*
   Vector(std::initializer_list<double> lst)
     :elem{new double[lst.size()]},
-    sz{static_cast<int>(lst.size())}
+    _size{static_cast<int>(lst.size())},
+    // capacity must be allocated based on size anyways, so why is the resize impl in push_back? bad.
+    // TODO
+    _capacity{0}
   {
     std::copy(lst.begin(),lst.end(),elem);
   }
+  */
 
   // ch 5.2.2
   Vector(const Vector& a);              // copy constructor
   Vector& operator=(const Vector& a);   // copy assignment
-
   Vector(Vector&& a);                   // move constructor
   // && is rvalue, no one else can assign to it, so we can steal its value
   // IMPORTANT: move is initiated when rvalue is used in either: constructor OR right hand side of an argument
@@ -32,43 +40,36 @@ public:
   Vector& operator=(Vector&& a);        // move assignment
 
 
-  void push_back(double);
+  // put reserve() logic back where it came from
+  void push_back(double v) {
+    if (_size == _capacity) {
+      reserve(_capacity == 0 ? 8 : 2 * _capacity);
+    }
+  }
 
+  int size() const {
+    return _size;
+  }
+  int capacity() const {
+    return _capacity;
+  }
+
+  void reserve(int newCapacity) {
+    if (newCapacity > _capacity) {
+      double* newElem = new double[newCapacity];
+      // slice convention of adding an int to a begin to get the desired end
+      std::copy(elem, elem + _size, newElem);
+      delete[] elem;
+      elem = newElem;
+      _capacity = newCapacity;
+    }
+  }
 
   ~Vector() { delete[] elem; }
 
   double& operator[](int i);
-  int size() const;
 private:
   double* elem;
-  int sz;
+  int _size;
+  int _capacity;
 };
-
-/** Ch 10 Vector Template
- */
-/*
-template<typename T>
-class Vector {
-  T* elem;
-  T* space;
-  T* last;
-public:
-  int size();
-  int capacity();
-
-  void reserve(int newsz);
-
-  void push_back(const T& t);
-  void push_back(T&& t);
-}
-
-// why does he include this again
-template<typename T>
-void vector<t>::push_back(const t& t)
-{
-  if (capacity()<size()+1)
-    reserve(size()==0?8:2*size());
-  new(space) T{t};
-  ++space;
-}
-*/
